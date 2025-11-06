341c66123755b5749f2f1193c4a6b85e6c9b648e
Carlos Patrick <cjp424@gmail.com>
2025-11-05 19:19:23 -0500

    first commit: fixed projects section

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,32 @@
 class HomeController < ApplicationController
     def index
-        @hello_world = "TEsting"
+        @projects = Project.all
+        @tasks = Task.all
     end
-end
+
+    def create_item
+        name = params[:name]
+        type = params[:type]
+        
+        if type == "projects"
+            @project = Project.create(name: name)
+
+            if @project.persisted?
+                redirect_to root_path, notice: 'Project created!'
+            else 
+                redirect_to root_path, alert: 'Error creating project.'
+            end
+        elsif type == "tasks"
+            project_id = params[:project_id]
+            start_time = Time.now
+            end_time = Time.now
+
+            @task = Task.create(name: name, project_id: project_id, start_time: start_time, end_time: end_time)
+            if @task.persisted?
+                redirect_to root_path, notice: 'Task Created!'
+            end
+        else
+            redirect_to root_path, alert: 'Invalid type specified.'
+            end
+        end
+    end
